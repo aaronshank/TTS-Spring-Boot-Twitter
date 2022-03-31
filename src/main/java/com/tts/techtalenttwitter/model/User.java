26e79536ee0abf8c318deeaf860f5026a2a6b961
Aaron Shank <shank@ctwo.net>
2022-03-31 11:34:35 -0400

    added follow function

--- src/main/java/com/tts/techtalenttwitter/model/User.java
+++ src/main/java/com/tts/techtalenttwitter/model/User.java
@@ -1,6 +1,7 @@
 package com.tts.techtalenttwitter.model;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -21,21 +22,28 @@ import org.hibernate.annotations.CreationTimestamp;
 @NoArgsConstructor
 @Entity
 public class User {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "user_id")
-	private Long id;
-	private String email;
-	private String username;
-	private String password;
-	private String firstName;
-	private String lastName;
-	private int active;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "user_id")
+  private Long id;
+  private String email;
+  private String username;
+  private String password;
+  private String firstName;
+  private String lastName;
+  private int active;
 
-	@CreationTimestamp
-	private Date createdAt;
+  @CreationTimestamp
+  private Date createdAt;
 
-	@ManyToMany(cascade = CascadeType.ALL)
-	@JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
-	private Set<Role> roles;
+  @ManyToMany(cascade = CascadeType.ALL)
+  @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+  private Set<Role> roles;
+
+  @ManyToMany(cascade = CascadeType.ALL)
+  @JoinTable(name = "user_follower", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "follower_id"))
+  private List<User> followers;
+
+  @ManyToMany(mappedBy = "followers")
+  private List<User> following;
 }
